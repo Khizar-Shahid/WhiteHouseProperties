c1adc5fba8a157d16c0bc54ad8dc16ed939834cc
Khizar-Shahid <shahidkhizar1403@gmail.com>
2025-07-26 20:58:52 +0330

    Update GTM integration with new container ID GTM-MMSLS7FT and proper head placement

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,31 +15,34 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <head>
         {/* Google Tag Manager */}
         <Script
           id="gtm"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MTP7Z87F');
+              })(window,document,'script','dataLayer','GTM-MMSLS7FT');
             `,
           }}
         />
-        
+        {/* End Google Tag Manager */}
+      </head>
+      <body>
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MTP7Z87F"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MMSLS7FT"
             height="0" 
             width="0" 
             style={{display:'none',visibility:'hidden'}}
           />
         </noscript>
+        {/* End Google Tag Manager (noscript) */}
         
         {children}
       </body>
